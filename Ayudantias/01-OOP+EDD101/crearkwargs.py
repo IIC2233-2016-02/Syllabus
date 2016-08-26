ab0c38268827789fbc4f4e04ac0f6e1bf47f761d
Ivania Donoso <indonoso@uc.cl>
2016-08-26 12:10:39 -0300

    ordenando la carpeta de ayudantías

--- Ayudantias/01-OOP+EDD101/crearkwargs.py
+++ Ayudantias/01-OOP+EDD101/crearkwargs.py
@@ -1,13 +1,13 @@
 class Contacto:
 
-    def __init__(self, nombre, email):
+    def __init__(self, nombre, email, **kwargs):
         self.nombre = nombre
         self.email = email
 
 class Cliente(Contacto):
 
-    def __init__(self, telefono, nombre, email):
-        super().__init__(nombre, email)
+    def __init__(self, telefono, **kwargs):
+        super().__init__(**kwargs)
         self.telefono = telefono
 
-client = Cliente(27291673, 'Anders', 'mail@uc.cl')
+client = Cliente(telefono=27291673, nombre='Anders', email='mail@uc.cl', direccion=10)
--- Ayudantias/01-OOP+EDD101/herencia.py
+++ Ayudantias/01-OOP+EDD101/herencia.py
@@ -1,30 +1,34 @@
 class AddressHolder:
 
     def __init__(self, calle='', ciudad='', numero='', comuna='',**kwargs):
-        #print('Adress:')
-        #print(kwargs)
+        print('Adress:')
+        print(kwargs)
         super().__init__(**kwargs)
         self.calle = calle
         self.ciudad = ciudad
         self.comuna = comuna
         self.numero = numero
 
+class Persona:
 
-class Contacto:
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+class Contacto(Persona):
 
     def __init__(self, nombre = '', email = '', **kwargs):
-        #print('Contacto:')
-        #print(kwargs)
+        print('Contacto:')
+        print(kwargs)
         super().__init__(**kwargs)
         self.nombre = nombre
         self.email = email
 
 class Cliente(Contacto, AddressHolder):
 
-    def __init__(self, telefono='', **kwargs):
-        #print('Cliente:')
-        #print(kwargs)
-        super().__init__(**kwargs)
+    def __init__(self, telefono='', nombre='', **kwargs):
+        print('Cliente:')
+        print(kwargs)
+        super().__init__(nombre=nombre, **kwargs)
         self.telefono = telefono
 
 print(Cliente.__mro__)
@@ -32,4 +36,3 @@ print(Cliente.__mro__)
 c = Cliente(nombre = 'Juan Perez', email = 'jp@gmail.com', telefono = '23542331',
             calle = 'Pedro de Valdivia', numero = '231', comuna = 'Providencia', ciudad = 'Santiago')
 
-print("{}, {}, {}, {}".format(c.nombre, c.email, c.calle, c.comuna))
